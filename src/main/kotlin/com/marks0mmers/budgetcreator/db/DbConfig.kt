f35138407fd33565acc103dcb92467e88041206e
Mark Sommers <msommers@prometheusgroup.com>
2021-07-30 09:24:29 -0400

    fixed db url

--- src/main/kotlin/com/marks0mmers/budgetcreator/db/DbConfig.kt
+++ src/main/kotlin/com/marks0mmers/budgetcreator/db/DbConfig.kt
@@ -5,13 +5,20 @@ import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import io.ktor.application.*
 import org.jetbrains.exposed.sql.Database
+import java.net.URI
 
 @Suppress("EXPERIMENTAL_API_USAGE")
 object DbConfig {
     fun Application.dbConfig() {
+        val uri = environment.config.propertyOrNull("postgresql.connectionStringProd")?.getString()?.let(::URI)
+        val username = uri?.userInfo?.split(":")?.get(0)
+        val password = uri?.userInfo?.split(":")?.get(1)
+        val dbUrl = uri?.run { "jdbc:postgresql://$host:$port$path" }
         val db = HikariDataSource(HikariConfig().apply {
             driverClassName = "org.postgresql.Driver"
-            jdbcUrl = "jdbc:${environment.config.propertyOrNull("postgresql.connectionString")?.getString()}"
+            jdbcUrl = dbUrl ?: "${environment.config.propertyOrNull("postgresql.connectionString")?.getString()}"
+            this.username = username
+            this.password = password
             maximumPoolSize = environment.config.propertyOrNull("postgresql.threadPoolSize")?.getString()?.toIntOrNull() ?: 3
             isAutoCommit = false
             validate()
